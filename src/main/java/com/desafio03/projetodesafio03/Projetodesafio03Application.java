ad5eb594c0acf0f95212571fd17362daef3ea722
Gabriel Xavier <gabrielgx928@gmail.com>
2025-02-13 11:30:02 -0300

    First commit

---
+++ src/main/java/com/desafio03/projetodesafio03/Projetodesafio03Application.java
@@ -0,0 +1,13 @@
+package com.desafio03.projetodesafio03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetodesafio03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetodesafio03Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio03/projetodesafio03/Projetodesafio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio03.projetodesafio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetodesafio03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
